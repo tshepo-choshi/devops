0ef00df181d4cb8e2e7830dd18fb60feb9bbb7b5
tshepochoshi <tshepo.choshi@gmail.com>
2024-03-10 22:44:50 +0200

    commit

---
+++ src/main/java/com/devops/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.devops.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@GetMapping("/devops/")
+	public String devOps(){
+		return "Welcome to devops for developers: ";
+	}
+}
